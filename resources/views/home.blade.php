66083e6b213cf49d8d6bb7d676ed70d31fd5bcfb
Efrain Choque Solis <e.ce.max.ef@gmail.com>
2017-12-09 21:32:05 -0500

    Traduccion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Te Logueaste!
                 </div>
             </div>
         </div>
